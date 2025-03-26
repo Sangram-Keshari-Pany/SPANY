2156e9227a8a47e34c5ebae52aa2e25ae9037ef4
SANGRAM KESHARI PANY <sangrampany546@gmail.com>
2025-03-26 16:38:04 +0530

    new db

--- Backend/SPANY/SPANYAPP/apps.py
+++ Backend/SPANY/SPANYAPP/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SpanyappConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "SPANYAPP"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SPANYAPP'
